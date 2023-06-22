f360798b0fe440712279c7ae1b093ead58767526
Tanya Chisepo <tanyachisepo@meta.com>
2023-06-22 00:56:27 -0700

    Configured ProductCard Component and added required props

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -4,34 +4,39 @@ import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
 import axios from 'axios'
-
+import ProductGrid from ""
 import "./App.css"
 
 export default function App() {
+
+  const [products, setProducts] = React.useState([]);
+  const [error, setError] = React.useState('');
+
 
   React.useEffect(() => {
     const storeURL = 'https://codepath-store-api.herokuapp.com/store';
     const fetchData = async () => {
       try {
         const response = await axios.get(storeURL);
-        console.log("Success!");
-        console.log(response.data);
+        const productsArray = response.data
+        setProducts(productsArray)
+        console.log(productsArray);
       } catch (error) {
+        setError('Sorry. No products found')
         console.log("Error:", error);
       }
 
     };
     fetchData();
-  }
-  )
+  },[]); // Empty dependency array to ensure this effect only runs once on component mount
+  
 
   return (
     <div className="app">
       <BrowserRouter>
         <main>
           {/* YOUR CODE HERE! */}
-
-
+         
           <Navbar />
           <Sidebar />
           <Home />
---
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ProductCard = ({
+  product,
+  productId,
+  quantity,
+  handleAddItemToCart,
+  handleRemoveItemFromCart,
+  showDescription
+}) => {
+  const { id, name, price, image, description } = product;
+
+  const handleAddToCart = () => {
+    handleAddItemToCart(id);
+  };
+
+  const handleRemoveFromCart = () => {
+    handleRemoveItemFromCart(id);
+  };
+
+  return (
+    <div className="product-card">
+      <div className="media">
+        <Link to={`/products/${id}`}>
+          <img src={image} alt={name} />
+        </Link>
+      </div>
+      <h3 className="product-name">{name}</h3>
+      <p className="product-price">${price.toFixed(2)}</p>
+      {showDescription && <p className="product-description">{description}</p>}
+      <div className="product-actions">
+        <button className="add" onClick={handleAddToCart}>
+          Add to Cart
+        </button>
+        <button className="remove" onClick={handleRemoveFromCart}>
+          Remove from Cart
+        </button>
+      </div>
+      {quantity > 0 && <div className="product-quantity">{quantity}</div>}
+    </div>
+  );
+};
+
+export default ProductCard;
