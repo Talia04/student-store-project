01b2a52f5ec2dec8c9eca9d7dadb8c42beb8236e
Tanya Chisepo <tanyachisepo@meta.com>
2023-06-22 10:10:59 -0700

    Populated and styled NavBar

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,35 @@
 import * as React from "react"
 import "./Navbar.css"
+import { Icon } from '@iconify/react';
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <p>Navbar</p>
+      <div className="content">
+
+        <div className="logo">
+          <a href="/">
+            <img src="https://codepath-student-store-demo.surge.sh/assets/codepath.f1b3e41a.svg" alt="codepath logo"/></a>
+        </div>
+        <div className="socials">
+          {/* Add social icons */}
+          <div className="socials">
+            <Icon icon="mdi:twitter" color="white" />
+          </div>
+          <div className="socials">
+            <Icon icon="teenyicons:instagram-solid" color="white" />
+            </div>
+          <div className="socials">
+            <Icon icon="ic:outline-facebook" color="white" />
+          </div>
+        </div>
+        <ul className="links">
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About Us</a></li>
+          <li><a href="/contact">Contact Us</a></li>
+          <li><a href="/buy">Buy Now</a></li>
+        </ul>
+      </div>
     </nav>
   )
 }
