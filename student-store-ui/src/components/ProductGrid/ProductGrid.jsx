9f539236e9f41d6e498ca2ffe7f5d44ea81e451a
Tanya Chisepo <tanyachisepo@meta.com>
2023-06-22 00:59:52 -0700

    Rendered ProductCard Component in ProductGrid for each product

--- student-store-ui/src/components/ProductGrid/ProductGrid.jsx
+++ student-store-ui/src/components/ProductGrid/ProductGrid.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import ProductCard from './ProductCard';
 
-const ProductGrid = ({ children }) => {
-  return <div className="product-grid">{children}</div>;
+const ProductGrid = ({
+  products,
+  handleAddItemToCart,
+  handleRemoveItemFromCart
+}) => {
+  return (
+    <div className="product-grid">
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          product={product}
+          showDescription={false}
+          handleAddItemToCart={handleAddItemToCart}
+          handleRemoveItemFromCart={handleRemoveItemFromCart}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ProductGrid;
