e746bc5385a2f668b4c3ad09b08ca8c2fe09dbdf
Tanya Chisepo <tanyachisepo@meta.com>
2023-06-21 23:19:30 -0700

    GET request for store products from Store API in useEffect hook

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,21 +1,41 @@
 import * as React from "react"
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
+import axios from 'axios'
+
 import "./App.css"
 
 export default function App() {
+
+  useEffect(() => {
+    const storeURL = 'https://codepath-store-api.herokuapp.com/store';
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(storeURL);
+        console.log(response.data)
+      } catch (error) {
+        console.log("Error:", error)
+      }
+
+    }
+  }
+  )
+
   return (
     <div className="app">
       <BrowserRouter>
         <main>
           {/* YOUR CODE HERE! */}
+
+
           <Navbar />
           <Sidebar />
           <Home />
 
-          
+
 
         </main>
       </BrowserRouter>
