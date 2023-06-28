2834178121f5567a80bd45e85646f70a4e88a25d
Tanya Chisepo <tanyachisepo@meta.com>
2023-06-27 21:13:55 -0700

    Defined all additional functions and state variables

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,5 +1,7 @@
 import * as React from "react"
+import { useState, useEffect } from 'react'
 import { BrowserRouter , Routes, Route } from 'react-router-dom'
+import { useParams } from "react-router-dom"
 import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
@@ -11,6 +13,8 @@ import About from "../About/About.jsx"
 import Contact from "../Contact/Contact"
 import Footer from "../Footer/Footer"
 import ProductDetail from "../ProductDetail/ProductDetail"
+import ShoppingCart from "../ShoppingCart/ShoppingCart"
+import CheckoutForm from "../CheckoutForm/CheckoutForm"
 import "./App.css"
 
 export default function App() {
@@ -20,6 +24,16 @@ export default function App() {
   const [searchText, setSearchText] = React.useState("");
   const [error, setError] = React.useState('');
   const [isFetching, setIsFetching] = React.useState(true);
+  const [isOpen, setIsOpen] = React.useState(null);
+  const [shoppingCart, setShoppingCart] = useState([]);
+  const [checkoutForm, setCheckoutForm] = useState({
+    // Initial state for the checkout form fields
+    name: '',
+    email: '',
+    address: '',
+  }) 
+
+
 
   React.useEffect(() => {
     const storeURL = 'https://codepath-store-api.herokuapp.com/store';
@@ -39,19 +53,122 @@ export default function App() {
 
     };
     fetchData();
-    console.log(products);
   },[]); // Empty dependency array to ensure this effect only runs once on component mount
   
+ // Function to handle toggling the Sidebar's open/closed state
+  const handleOnToggle = () => {
+    setIsOpen(!isOpen);
+    if (isOpen){
+      console.log("Sidebar Open!")
+    }
+  };
+
+  // Function to handle updating the shopping cart state
+  const handleUpdateShoppingCart = (updatedCart) => {
+    setShoppingCart(updatedCart);
+    console.log(shoppingCart);
+  };
+  
+  // Function to handle updating the checkout form state
+  const handleUpdateCheckoutForm = (updatedForm) => {
+    setCheckoutForm(updatedForm);
+  };
+
+  // Function to handle adding an item to the shopping cart
+  const handleAddItemToCart = (productId) => {
+  const updatedCart = [...shoppingCart];
+  const existingItem = updatedCart.find((item) => item.itemId === productId);
+
+  
+  if (existingItem) {
+    existingItem.quantity += 1;
+  } else {
+    const product = products.find((item) => item.id === productId);
+    const newItem = {
+      itemId: productId,
+      quantity: 1,
+    };
+    updatedCart.push(newItem);
+    
+  }
+  setShoppingCart(updatedCart);
+  };
+
+  // Function to handle updating the quantity of an item in the shopping cart
+  const handleUpdateQuantity = (itemId, newQuantity) => {
+    const updatedCart = shoppingCart.map((item) => {
+      if (item.itemId === itemId) {
+        return { ...item, quantity: newQuantity };
+      }
+      return item;
+    });
+    setShoppingCart(updatedCart);
+  };
+
+
+  // Function to handle removing an item from the shopping cart
+  const handleRemoveItemFromCart = (productId) => {
+    const updatedCart = [...shoppingCart];
+    const existingItem = updatedCart.find((item) => item.itemId === productId);
+
+    if (existingItem) {
+      existingItem.quantity -= 1;
+
+      if (existingItem.quantity === 0) {
+        const itemIndex = updatedCart.findIndex((item) => item.itemId === productId);
+        updatedCart.splice(itemIndex, 1);
+      }
+
+    }
+
+    setShoppingCart(updatedCart);
+  };
+
+  // Function to handle updating the checkout form state
+  const handleOnCheckoutFormChange = (name, value) => {
+    setCheckoutForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
+
+  // Function to handle submitting the checkout form
+  const handleOnSubmitCheckoutForm = async () => {
+    const requestBody = {
+      user: {
+        name: checkoutForm.name,
+        email: checkoutForm.email,
+      },
+      shoppingCart: shoppingCart.map((item) => ({
+        itemId: item.itemId,
+        quantity: item.quantity,
+    })),
+  };
+  console.log("Success!")
+  };
+
+
   const handleSearch = (event) => {
     const filteredProducts = products.filter((product) =>
       product.name.toLowerCase().includes(searchText.toLowerCase())
     );
     setProducts(filteredProducts);
-  };
+  }
   
   return (
     <div className="app">
       <BrowserRouter>
+        <Sidebar 
+          isOpen={isOpen}
+          handleOnToggle={handleOnToggle}
+          shoppingCart={shoppingCart}
+          products={products}
+          checkoutForm={checkoutForm}
+          handleOnSubmitCheckoutForm={handleOnSubmitCheckoutForm}
+          
+        >
+
+        </Sidebar>
         <main>
           
           <Navbar />
@@ -61,16 +178,24 @@ export default function App() {
             handleSearch={handleSearch} 
             allProducts={allProducts}
             setProducts={setProducts}
-            updateText={setSearchText}
+
           />
 
           <Routes>
 
             <Route
-              path='/'  element={<Home products={products}/>}
+              path='/'  element={<Home products={products} handleAddItemToCart={handleAddItemToCart} handleRemoveItemFromCart ={handleRemoveItemFromCart}/>}
             />
             <Route 
-              path='/products/:productId' element={<ProductDetail products={products} isFetching={isFetching}/>}
+              path='/products/:productId' element={<
+                ProductDetail products={products}
+                isFetching={isFetching} 
+                quantity={
+                  shoppingCart.find((item) => item.itemId === useParams.productId)?.quantity || 0
+                }
+                handleAddItemToCart={handleAddItemToCart}
+                handleRemoveItemFromCart={handleRemoveItemFromCart}
+              />}
               />
           </Routes>
 
