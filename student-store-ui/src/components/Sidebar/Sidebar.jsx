8f93367115164a6939879eba98f28127c10d7e9f
Tanya Chisepo <tanyachisepo@meta.com>
2023-06-27 20:47:34 -0700

    Created Sidebar Component and passed required props

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,38 @@
-import * as React from "react"
-import "./Sidebar.css"
+import React from 'react';
+import ShoppingCart from '../ShoppingCart/ShoppingCart';
+import CheckoutForm from '../CheckoutForm/CheckoutForm';
+import MenuIcon from '@material-ui/core/Menu';
+import './Sidebar.css';
 
-export default function Sidebar() {
+const Sidebar = ({
+  isOpen,
+  shoppingCart,
+  products,
+  checkoutForm,
+  handleOnCheckoutFormChange,
+  handleOnSubmitCheckoutForm,
+  handleOnToggle,
+}) => {
   return (
-    <section className="sidebar">
-      <p>Sidebar</p>
+    <section className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
+      <button className="toggle-button" onClick={handleOnToggle}>
+        <i className="material-icons">
+          menu
+        </i>
+      </button>
+      
+      {isOpen && (
+        <>
+          <ShoppingCart className='shopping-cart' shoppingCart={shoppingCart} products={products}  />
+          <CheckoutForm className='checkout-form'
+            checkoutForm={checkoutForm}
+            handleOnCheckoutFormChange={handleOnCheckoutFormChange}
+            handleOnSubmitCheckoutForm={handleOnSubmitCheckoutForm}
+          />
+        </>
+      )}
     </section>
-  )
-}
+  );
+};
+
+export default Sidebar;
